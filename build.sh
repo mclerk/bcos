7399a65d4677f9af62f3961431a0569c159c4b93
Michael Clerk <me@mclerk.net>
2024-10-28 17:09:32 -0400

    Initial Commit

--- build.sh
+++ build.sh
@@ -13,11 +13,11 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install bat eza fd-find git git-lfs helix nvtop syncthing tmux zoxide zsh zsh-autosuggestions zsh-syntax-highlighting
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
